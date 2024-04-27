ba967e13a3c8e8d09c31a7044f6c22361ec42194
juane747 <emifive67@gmail.com>
2024-04-26 20:52:17 -0600

    Optimizacion de interface

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,26 +1,30 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './Item.scss'
-
+import { CardBody } from 'react-bootstrap';
 function Item() {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card>
       <Card.Body>
-      <Card.Text  className='fw-bold'>
-        Name
-      </Card.Text>
-        <Card.Text>Realizar Proyecto</Card.Text>
-        <Card.Text className='fw-bold'>
-          Descripcion
-        </Card.Text>
-        <Card.Text>
-          Elaborar proyecto del curso de DAW.
-        </Card.Text>
-        <Button variant="info">Eliminar</Button>
-        <Button varian="info">Editar</Button>
+        <Card.Title>Proyecto del Curso de Desarrollo Web</Card.Title>
+          <Card.Text className='fw-bold'>
+            Descripcion
+          </Card.Text>
+          <Card.Text>
+            Elaborar una aplicacion web responsive en la que se pueda llenar el control de mis tareas
+          </Card.Text>
+          <Card.Text className='fw-bold'>
+            Due Date
+          </Card.Text>
+          <Card.Text>
+            31/05/2024
+          </Card.Text>
+      </Card.Body>
+        <Card.Body>    
+          <Button variant="info">Editar</Button>
+          <Button varian="info">Eliminar</Button>
       </Card.Body>
     </Card>
   );
 }
-
 export default Item;
